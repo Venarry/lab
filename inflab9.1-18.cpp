2352384ee888a2aa1f3b62e8bdece28f6f6d4bd8
Venarry <venarry@mail.ru>
2020-07-02 00:04:20 +0300

    refresh file

--- inflab9.1-18.cpp
+++ inflab9.1-18.cpp
@@ -1,15 +1,14 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <locale.h>
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 using namespace std;
 
-
 int main(void)
 {
 	setlocale(LC_CTYPE, "");
-	int i,j, n, m ,summ,sred[50]; 
+	int i,j,j1, n, m ,x,summ,sred[50]; 
 	printf("Введите размерность массива\n");
 	cin >> n;
 	cin >> m;
@@ -25,23 +24,14 @@ int main(void)
 		}
 	}
 
-	for (i = 0; i < n; i++)
-	{
-		summ = 0;
-		for (j = 0; j < m; j++)
-		{
-			summ += arr[i][j];
-		}
-		sred[i] = summ / m;
-	}
 
-	printf("Массив a:\n");
+	printf("Исходный Массив a:\n");
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < m; j++)
 		{
 			printf("%d ", arr[i][j]);
-			if (j == m-1)
+			if (j == m - 1)
 			{
 				printf("\n");
 			}
@@ -50,13 +40,35 @@ int main(void)
 
 
 	for (i = 0; i < n; i++)
-		printf("Среднее значение %d строки:%d\n", i + 1, sred[i]);
-	for (i = 0; i < n; i++)
-		for (j=i+1; j < m ; j++)
+	{
+		for (j = 0; j < m; j++)
 		{
-			if (sred[i] == sred[j])
-				printf("Совпадают %d и %d строки\n", i + 1, j + 1);
+			if ((arr[i][j] == 0) and (j>0))
+			{
+				j1 = j;
+				while (j1 > 0 )
+				{
+					x = arr[i][j1];
+					arr[i][j1] = arr[i][j1 - 1];
+					arr[i][j1 - 1] = x;
+					j1--; 
+				}
+			}
 		}
+	}
+
+	printf("Конечный Массив a:\n");
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			printf("%d ", arr[i][j]);
+			if (j == m - 1)
+			{
+				printf("\n");
+			}
+		}
+	}
 
 	delete[] arr;
-}*/
+}
